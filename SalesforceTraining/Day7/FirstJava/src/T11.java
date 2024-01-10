c57701d910ce570fe85a5daad5f338af61cb3069
patelpratham4518 <patelpratham4518@gmail.com>
2024-01-10 18:06:03 +0530

    final reverse decimal

--- SalesforceTraining/Day7/FirstJava/src/T11.java
+++ SalesforceTraining/Day7/FirstJava/src/T11.java
@@ -9,23 +9,25 @@ public class T11 {
         float number = scanner.nextFloat();
         String reverse = "";
         String str = Float.toString(number);
-        // System.out.println(str);
+        
         String[] array = str.split("\\.");
         int i = str.indexOf('.');
 
-        //new
+        
         StringBuilder sb = new StringBuilder(array[0]);
         sb.reverse();
 
         reverse = array[1] + sb.toString();
 
 
-        // reverse = array[1] + array[0];
-        // reverse = reverse.substring(0,i) + "." + reverse.substring(i,reverse.length());
+        
         StringBuilder addDot = new StringBuilder(reverse);
         addDot.insert(i, ".");
         reverse = addDot.toString();
-        System.out.println(reverse);
+
+        Float reversedDecimal = Float.parseFloat(reverse);
+
+        System.out.println(reversedDecimal);
         scanner.close();
 
     }
