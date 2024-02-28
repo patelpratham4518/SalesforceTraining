313039e0394245af90daec409d24cf324d5af258
patelpratham4518 <patelpratham4518@gmail.com>
2024-02-28 16:26:08 +0530

    first comp working

--- SalesforceTraining/MyOrg/force-app/main/default/aura/createContact/createContactController.js
+++ SalesforceTraining/MyOrg/force-app/main/default/aura/createContact/createContactController.js
@@ -1,34 +1,40 @@
 ({
     doInit : function(component, event, helper) {
-        // var action = component.get("c.getAccounts");
-        // action.setCallback(this, function(response){
-        //     var state = response.getState();
-        //     if(state == 'SUCCESS') {
-        //         component.set("v.accList",response.getReturnValue());
-        //         console.log("AccList = "+component.get("v.accList"));
-        //     }
-        // });
-        // $A.enqueueAction(action);
+        
     },
     createContact : function (component , event , helper) {
         var FirstName = component.get("v.FirstName");
         var LastName = component.get("v.LastName");
         var Mobile = component.get("v.Mobile");
         var Email = component.get("v.Email");
+        var accountId = component.get("v.AccountId");
     
         
         var action = component.get("c.createContactApex");
-        action.setParams({FirstName:FirstName , LastName:LastName , Mobile:Mobile, Email:Email});
+        action.setParams({FirstName:FirstName , LastName:LastName , Mobile:Mobile, Email:Email, accountId:accountId});
         action.setCallback(this, function(response){
             var state = response.getState();
             if(state == 'SUCCESS') {
-                alert(response.getReturnValue());
+                // alert(response.getReturnValue());
+                var toastEvent = $A.get("e.force:showToast");
+                console.log("action =  "+toastEvent);
+                toastEvent.setParams({
+                    title : 'Success',
+                    message: 'This is a success message',
+                    duration:' 5000',
+                    key: 'info_alt',
+                    type: 'success',
+                    mode: 'pester'
+                    
+                });
+                toastEvent.fire();
             }
         });
         $A.enqueueAction(action);
     },
     showSuccess : function(component, event, helper) {
         console.log("success called");
+       
         var toastEvent = $A.get("e.force:showToast");
         console.log("action =  "+toastEvent);
         toastEvent.setParams({
@@ -43,47 +49,48 @@
         toastEvent.fire();
     },
 
-    // search : function (component , event , helper) {
+    search : function (component , event , helper) {  
+
+        var searchString = component.get("v.searchString");
         
-    //     var searchString = component.get("v.searchString");
-    //     if (searchString.length >= 0) {
-    //         var action = component.get("c.getAccounts");
-    //        console.log("Search Records: ", action);
-    //         action.setParams({
-    //             searchString: searchString
-    //         });
-    //         action.setCallback(this, function(response) {
-    //             var state = response.getState();
-    //             if (state === "SUCCESS") {
-    //                 component.set("v.accList", response.getReturnValue());
-    //                 console.log("Returned accounts: ", response.getReturnValue());
-    //             }
-    //         });
-    //         $A.enqueueAction(action);
-    //     } else {
-    //         component.set("v.accList", []);
-    //         console.log("Cleared records");
-    //     } 
-    // },
+        
+        if (searchString.length > 0) {
+            
+            var action = component.get("c.getAccounts");
+            action.setParams({
+                searchString: searchString
+            });
+            action.setCallback(this, function(response) {
+                var state = response.getState();
+                if (state === "SUCCESS") {
+                    component.set("v.accList", response.getReturnValue());
+                }
+            });
+            $A.enqueueAction(action);
+        } else {
+            component.set("v.accList", []);
+            console.log("Cleared records");
+        } 
+    },
 
-    // clearSearch : function (component , event , helper) {
-    //     component.set("v.searchString", "");
-    //     component.set("v.accList",[]);
-    // },
-    // selectRecord : function (component , event , helper) {
-    //     var selectedRecordId = event.currentTarget.dataset.record;
-    //     var records = component.get("v.accList");
+    clearSearch : function (component , event , helper) {
+        component.set("v.searchString", "");
+        component.set("v.accList",[]);
+    },
+    selectRecord : function (component , event , helper) {
+        var selectedRecordId = event.currentTarget.dataset.record;
+        var records = component.get("v.accList");
 
-    //     var selectedRecord = records.find(function(record) {
-    //         console.log('selectRecord@@:  '+record.Id);
-    //         return record.Id === selectedRecordId;
-    //     });
+        var selectedRecord = records.find(function(record) {
+            console.log('selectRecord@@:  '+record.Id);
+            return record.Id === selectedRecordId;
+        });
 
-    //     console.log("RecordId = " + selectedRecordId);
-    //     component.set("v.AccountId",selectedRecordId);
-    //     component.set("v.searchString",selectedRecord.Name);
-    //     component.set("v.accList",[]);
-    // }
+        console.log("RecordId = " + selectedRecordId);
+        component.set("v.AccountId",selectedRecordId);
+        component.set("v.searchString",selectedRecord.Name);
+        component.set("v.accList",[]);
+    }
     
     
 })
