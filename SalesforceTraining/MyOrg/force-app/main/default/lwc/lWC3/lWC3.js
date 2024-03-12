dee5e09851ddcd9a7771ee743f99afe13dd2d9a2
patelpratham4518 <patelpratham4518@gmail.com>
2024-03-12 10:36:06 +0530

    Multiple uploads working

--- SalesforceTraining/MyOrg/force-app/main/default/lwc/lWC3/lWC3.js
+++ SalesforceTraining/MyOrg/force-app/main/default/lwc/lWC3/lWC3.js
@@ -5,11 +5,18 @@ export default class LWC3 extends LightningElement {
    @track attachmentList = []
     handleChange(event){
         console.log("handleChange called");
-        let attachments = this.template.querySelector('[data-id="attachments"]').files[0]
-        
-        attachments.index = this.attachCount++
-        attachments.url = URL.createObjectURL(attachments)
-        this.attachmentList.push(attachments)
+        // let attachments = this.template.querySelector('[data-id="attachments"]').files[0]
+        // attachments.index = this.attachCount++
+        // attachments.url = URL.createObjectURL(attachments)
+        // this.attachmentList.push(attachments)
+
+        let attachments = this.template.querySelector('[data-id="attachments"]').files
+        for (let attach of attachments) {
+            attach.index = this.attachCount++
+            attach.url = URL.createObjectURL(attach)
+            this.attachmentList.push(attach)
+        }
+
       
     }
 
