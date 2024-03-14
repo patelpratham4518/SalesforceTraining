503f27d4bd30caaf4ee3d6dc0bb0a6d9da1019ff
patelpratham4518 <patelpratham4518@gmail.com>
2024-03-14 10:33:12 +0530

    LWC 5 First Section done

---
+++ SalesforceTraining/MyOrg/force-app/main/default/lwc/lWC5/lWC5.js
@@ -0,0 +1,118 @@
+import { LightningElement,track ,wire} from 'lwc';
+import getObjectList from '@salesforce/apex/LWC5.getObjectList'
+import getEmailId from '@salesforce/apex/LWC5.getEmailId'
+
+export default class LWC5 extends LightningElement {
+
+
+    @track currentStep = '1';
+ 
+    handleOnStepClick(event) {
+        this.currentStep = event.target.value;
+    }
+ 
+    get isStepOne() {
+        return this.currentStep === "1";
+    }
+ 
+    get isStepTwo() {
+        return this.currentStep === "2";
+    }
+ 
+    get isStepThree() {
+        return this.currentStep === "3";
+    }
+ 
+    get isEnableNext() {
+        return this.currentStep != "3";
+    }
+ 
+    get isEnablePrev() {
+        return this.currentStep != "1";
+    }
+ 
+    get isEnableFinish() {
+        return this.currentStep === "3";
+    }
+ 
+    handleNext(){
+        if(this.currentStep == "1"){
+            this.currentStep = "2";
+        }
+        else if(this.currentStep = "2"){
+            this.currentStep = "3";
+        }
+    }
+ 
+    handlePrev(){
+        if(this.currentStep == "3"){
+            this.currentStep = "2";
+        }
+        else if(this.currentStep = "2"){
+            this.currentStep = "1";
+        }
+    }
+ 
+    handleFinish(){
+        alert("Email sent !!!")
+    }
+
+    objects = [
+        {
+            label : "Account",
+            value : "Account"
+        },
+        {
+            label : "Contact",
+            value : "Contact",
+        },
+        {
+            label : "Lead",
+            value : "Lead"
+        }
+    ]
+
+    objectType
+    @track objectList
+    @track iconName
+    selectedRecordsEmail = new Map()
+    
+
+    handleObjectSelection(event){
+        this.objectType = event.detail.value
+        this.iconName = `standard:${this.objectType}`
+        getObjectList({
+            objectType : this.objectType
+        }).then(response => {
+            this.objectList = response
+        }).catch(error => {
+            console.log("Error = ",error);
+        })
+
+    }
+
+    selectRecord(event){
+        
+        let selectedRecord = event.target.value
+        if (event.target.checked) {
+
+            getEmailId({
+                objectType : this.objectType,
+                recordId : event.target.value
+            }).then(email => {
+                if (email) {
+                    this.selectedRecordsEmail.set(selectedRecord,email)
+                }
+                
+            }).catch(error => {
+                console.log("Error = ",error);
+            })
+
+        }
+        else{
+            this.selectedRecordsEmail.delete(selectedRecord)
+        }
+    }
+
+
+}
