d9c6074b90f2d3c58f6b8edd9381346510632ff0
patelpratham4518 <patelpratham4518@gmail.com>
2024-03-15 12:52:32 +0530

    Getting map of sosl search from apex

---
+++ SalesforceTraining/MyOrg/force-app/main/default/lwc/lWC6/lWC6.js
@@ -0,0 +1,57 @@
+import { LightningElement, track } from 'lwc';
+import getRecords from '@salesforce/apex/LWC6.getRecords'
+
+
+export default class LWC6 extends LightningElement {
+    selected = [];
+    searchValue
+    responseRecords
+    responseRecordsKeys = []
+
+    get options() {
+        return [
+            { label: 'Account', value: 'Account' },
+            { label: 'Contact', value: 'Contact' },
+            { label: 'Opportunity', value: 'Opportunity' },
+            { label: 'Lead', value: 'Lead' },
+            { label: 'Student', value: 'Student__c' },
+            { label: 'Club', value: 'Club__c' },
+            { label: 'Course', value: 'Course__c' },
+            { label: 'Debit', value: 'Debit__c' },
+            { label: 'Employee', value: 'Employee__c' },
+            { label: 'Error_log', value: 'Error_log__c' },
+        ];
+    }
+
+    get selected() {
+        return this.selected.length ? this.selected : 'none';
+    }
+
+    handleChange(e) {
+        this.selected = e.detail.value;
+    }
+
+    searchKeyword(event) {
+        this.searchValue = event.target.value;
+        
+    }
+    handleSearchKeyword(){
+        console.log(this.searchValue);
+        console.log(this.selected);
+        getRecords({
+            searchValue : this.searchValue,
+            selected : this.selected
+        }).then(response => {
+            console.log('Response = ',response);
+            this.responseRecords = response
+            this.responseRecordsKeys = Object.getOwnPropertyNames(this.responseRecords)
+        }).catch(error => {
+            console.log('Error message : ',error);
+        })
+    }
+    
+}
+
+        
+    
+
